8a6c807ad342257dadd6e3810ccd98178aac2e42
vienie <duvien@gmail.com>
2021-07-23 09:05:17 +0100

    Add convert_images.py and README.md

---
+++ Automating Real-World Tasks with Python/Week 1 - Manipulating Images/convert_images.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+
+from PIL import Image
+newsize = 128, 128
+
+for file in glob.glob("ic_*"):
+       im = Image.open(file).convert('RGB')
+       im.rotate(270).resize((newsize)).save("/opt/icons/" + file, "JPEG")
+
